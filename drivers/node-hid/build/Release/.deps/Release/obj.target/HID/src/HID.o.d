451093b80f28721547ac496043bdc97a15e1b3c2
ajfisher <ajfisher.td@gmail.com>
2015-07-11 19:08:32 +0800

    Made wireless module work

---
+++ drivers/node-hid/build/Release/.deps/Release/obj.target/HID/src/HID.o.d
@@ -0,0 +1,38 @@
+cmd_Release/obj.target/HID/src/HID.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/ajfisher/.node-gyp/0.12.2/src -I/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include -I/Users/ajfisher/.node-gyp/0.12.2/deps/v8/include -I../hidapi/hidapi -I../node_modules/nan  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/HID/src/HID.o.d.raw  -c -o Release/obj.target/HID/src/HID.o ../src/HID.cc
+Release/obj.target/HID/src/HID.o: ../src/HID.cc \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8stdint.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8config.h \
+  /Users/ajfisher/.node-gyp/0.12.2/src/node.h \
+  /Users/ajfisher/.node-gyp/0.12.2/src/node_version.h \
+  /Users/ajfisher/.node-gyp/0.12.2/src/node_buffer.h \
+  /Users/ajfisher/.node-gyp/0.12.2/src/smalloc.h \
+  ../hidapi/hidapi/hidapi.h ../node_modules/nan/nan.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-errno.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-version.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-unix.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-threadpool.h \
+  /Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-darwin.h \
+  /Users/ajfisher/.node-gyp/0.12.2/src/node_object_wrap.h \
+  ../node_modules/nan/nan_new.h \
+  ../node_modules/nan/nan_implementation_12_inl.h
+../src/HID.cc:
+/Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8stdint.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/v8/include/v8config.h:
+/Users/ajfisher/.node-gyp/0.12.2/src/node.h:
+/Users/ajfisher/.node-gyp/0.12.2/src/node_version.h:
+/Users/ajfisher/.node-gyp/0.12.2/src/node_buffer.h:
+/Users/ajfisher/.node-gyp/0.12.2/src/smalloc.h:
+../hidapi/hidapi/hidapi.h:
+../node_modules/nan/nan.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-errno.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-version.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-unix.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-threadpool.h:
+/Users/ajfisher/.node-gyp/0.12.2/deps/uv/include/uv-darwin.h:
+/Users/ajfisher/.node-gyp/0.12.2/src/node_object_wrap.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
