1cb3ef4e6245e2f5e675beddc6fd26918f2eeabd
ajfisher <ajfisher.td@gmail.com>
2015-11-09 17:40:24 +1100

    Fixes to make usb and bt firmatas available - closes #9

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,31 +6,82 @@ module.exports = function(grunt) {
     grunt.initConfig({
         // exec: create task dynamically see end of file for where this happens.
         exec: {
-            compile: {
+            compile_usb: {
                 command: function() {
                     return arduino + " --verify --verbose-build --board arduino:avr:uno" + 
-                    " --pref build.path=firmware/bin/uno/ firmware/mbotFirmata/mbotFirmata.ino";
+                    " --pref build.path=firmware/bin/usb/uno/ firmware/build/usb/mbotFirmata/mbotFirmata.ino";
+                },
+            },
+            compile_bluetooth: {
+                command: function() {
+                    return arduino + " --verify --verbose-build --board arduino:avr:uno" + 
+                    " --pref build.path=firmware/bin/bluetooth/uno/ firmware/build/bluetooth/mbotFirmata/mbotFirmata.ino";
                 },
             },
         },
+        'string-replace': {
+            precompile: {
+                files: [{
+                    src: 'firmware/build/bluetooth/mbotFirmata/mbotFirmata.ino',
+                    dest: 'firmware/build/bluetooth/mbotFirmata/mbotFirmata.ino',
+                }],
+                options: {
+                    replacements: [{
+                        pattern: /57600/,
+                        replacement: '115200',
+                    }],
+                },
+            },
+        },
+        copy: {
+            options: {
+                timestamp: true,
+            },
+
+            firmata_usb: {
+                cwd: 'firmware/src/',
+                flatten: true,
+                src: [ 'mbotFirmata/*' ],
+                dest: 'firmware/build/usb/mbotFirmata/',
+                expand: true,
+                filter: 'isFile',
+            },
+            firmata_bluetooth: {
+                cwd: 'firmware/src/',
+                flatten: true,
+                src: [ 'mbotFirmata/*' ],
+                dest: 'firmware/build/bluetooth/mbotFirmata/',
+                expand: true,
+                filter: 'isFile',
+            },
+        },
         clean: {
+            firmware_build: {
+                src: [  
+                        'firmware/build/usb',
+                        'firmware/build/bluetooth',
+                     ]
+            },
             compiled_bins: {
                 src: [
-                        'firmware/bin/uno/*',
+                        'firmware/bin/*',
                     ]
             },
             post_compile: {
                 src: [
-                        'firmware/bin/uno/!(*ino.hex)'
+                        'firmware/bin/usb/uno/!(*ino.hex)',
+                        'firmware/bin/bluetooth/uno/!(*ino.hex)'
                     ]
             },
         },
     });
  
     // load the tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-string-replace');
 
-    grunt.registerTask('build', ['clean:compiled_bins']);
+    grunt.registerTask('build', ['clean:firmware_build', 'clean:compiled_bins', 'copy', 'string-replace']);
     grunt.registerTask('compile', ['build', 'exec', 'clean:post_compile']);
 };
